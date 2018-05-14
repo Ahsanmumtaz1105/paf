5fa4b2f3e619815b56c4460cfaee5192a0c98972
Ahsanmumtaz1105 <31079318+Ahsanmumtaz1105@users.noreply.github.com>
2018-05-14 19:30:42 +0530

    Update CreateCredFile.ps1

--- setup_infra/CreateCredFile.ps1
+++ setup_infra/CreateCredFile.ps1
@@ -1,3 +1,3 @@
 # Create a encrypted credential file to authenticate to Vsphere Server
-Connect-VIServer -Server 10.17.6.250
-New-VICredentialStoreItem -Host 10.17.6.250 -User NDI\ahsan_mumtaz -Password XYZ -File C:\git-repo\ivt_framework\AutomationPOC\setup_infra\In_creds
+Connect-VIServer -Server 10.x.x.x
+New-VICredentialStoreItem -Host 10.x.x.x -User username -Password XYZ -File C:\git-repo\ivt_framework\AutomationPOC\setup_infra\In_creds
